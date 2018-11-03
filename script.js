ce73db60c0b8f79f9559440a506ce25309b81409
Joxit <joxit972@gmail.com>
2018-11-03 12:16:33 +0100

    First page with portrait

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById('portrait').addEventListener('click', function(e) {
+  const widths = ['75%', '100%', '90%']
+  const menu = document.getElementById('menu');
+  // menu.style.display = (menu.style.display == "" || menu.style.display === 'none') ? 'flex' : 'none';
+
+  const lines = menu.getElementsByClassName('menu-line');
+  for (let i = 0; i < lines.length; i++) {
+    if (lines.item(i).style.width == '0px' || lines.item(i).style.width == '') {
+      lines.item(i).style.transition = 'width 1s';
+      lines.item(i).style.width = widths[i];
+
+    } else {
+      lines.item(i).style.transition = '';
+      lines.item(i).style.width = '0px';
+
+    }
+  }
+    const items = menu.getElementsByClassName('item');
+    for (let i = 0; i < items.length; i++) {
+      items.item(i).style.opacity = (items.item(i).style.display == '' || items.item(i).style.display === 'none') ? '1' : '0';
+      items.item(i).style.transition = (items.item(i).style.display == '' || items.item(i).style.display === 'none') ? 'opacity 2s' : '';
+      items.item(i).style.display = (items.item(i).style.display == '' || items.item(i).style.display === 'none') ? 'block' : 'none';
+    }
+});
